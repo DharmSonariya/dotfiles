989d8e153adc531f9a4dfd1298791095e93ad7b7
Dharm Sonariya <dharmsonariya@outlook.com>
2020-06-08 00:26:23 +0000

    rmoved VS Code extensions and add sleep 5 to suso_init.sh

--- sudo_init.sh
+++ sudo_init.sh
@@ -23,27 +23,6 @@ else
     yum -y install code
 fi
 ##############################################################################################################
-# https://docs.aws.amazon.com/toolkit-for-vscode/latest/userguide/setup-toolkit.html
-# https://marketplace.visualstudio.com/itemdetails?itemName=AmazonWebServices.aws-toolkit-vscode
-
-echo 'Installing VS Code extensions'
-sleep 2
-code --install-extension AmazonWebServices.aws-toolkit-vscode \
-    --install-extension teabyii.ayu \
-    --install-extension rogalmic.bash-debug \
-    --install-extension CoenraadS.bracket-pair-colorizer-2 \
-    --install-extension streetsidesoftware.code-spell-checker \
-    --install-extension ms-azuretools.vscode-docker \
-    --install-extension rubbersheep.gi \
-    --install-extension donjayamanne.githistory \
-    --install-extension ms-vscode.theme-predawnkit \
-    --install-extension ms-python.python \
-    --install-extension Shan.code-settings-sync \
-    --install-extensione vsc-python-indent \
-    --install-extension foxundermoon.shell-format \
-    --install-extension Remisa.shellman
---install-extension mauve.terraform
-##############################################################################################################
 echo 'Installing Google Chrome with the key and repository'
 
 sleep 2
@@ -53,6 +32,7 @@ yum install -y ./google-chrome-stable_current_*.rpm
 # https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html#install-plugin-linux
 
 echo Install Session Manager Plugin on Linux
+sleep 5
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
 yum install -y session-manager-plugin.rpm
 ##############################################################################################################
