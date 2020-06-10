64a39e3b3408f7042ebbff803689e406bea5269d
Dharm Sonariya <dharmsonariya@outlook.com>
2020-06-10 00:08:19 +0000

    add set -x, change sleep time and sequence yum install

--- sudo_init.sh
+++ sudo_init.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/bin/bash -xe
+set -x
 
 ##############################################################################################################
 # This script installs Linux Packages and it's require sudo privileged
@@ -6,14 +7,14 @@
 echo "Updating installed packages, enabling EPEL repo and installing yum packages"
 sleep 2
 
-yum update -y && yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && yum groupinstall -y 'Development Tools' && yum install -y \
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && yum update -y && yum groupinstall -y 'Development Tools' && yum install -y \
     perl-devel \
     git \
     ncurses-devel \
     adobe-source-code-pro-fonts
 ##############################################################################################################
 echo 'Installing Visual Studio Code with the key and repository'
-sleep 2
+sleep 10
 if [ -x "$(command -v code)" ]; then
     echo '✔️ code installed'
 else
@@ -26,14 +27,14 @@ fi
 ##############################################################################################################
 echo 'Installing Google Chrome with the key and repository'
 
-sleep 2
+sleep 10
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 yum install -y ./google-chrome-stable_current_*.rpm
 ##############################################################################################################
 # https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html#install-plugin-linux
 
 echo Install Session Manager Plugin on Linux
-sleep 5
+sleep 10
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
 yum install -y session-manager-plugin.rpm
 ##############################################################################################################
