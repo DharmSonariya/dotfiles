a1f308af031d172e16e160df3bdb3f1e81e260d2
Dharm Sonariya <dharmsonariya@outlook.com>
2020-06-10 07:24:45 +0000

    remov sudo and tansfer ssh from add_user.sh to ec2_init.sh

--- add_user.sh
+++ add_user.sh
@@ -13,24 +13,21 @@ read -r username
 export USERNAME="$username"
 
 echo Add new users
-sudo adduser "$USERNAME"
-sudo passwd "$USERNAME"
-sudo usermod -aG wheel "$USERNAME"
+adduser "$USERNAME"
+passwd "$USERNAME"
+usermod -aG wheel "$USERNAME"
 echo "User has been added to wheel group"
 
-echo Add known SSH authorize key, this is specially require for EC2 instance
-sudo cp -r /home/"$ORIGINAL_USER"/.ssh/ /home/"$USERNAME"/ && sudo chown -R "$USERNAME":"$USERNAME" /home/"$USERNAME"/.ssh
-
 echo Invoke sudo_init.sh invoke user_init.sh to /home/"$USERNAME"
-curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/sudo_init.sh | sudo bash \
+curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/sudo_init.sh \
 && /bin/su -c "cd  /home/$USERNAME/ && sh -c $(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/user_init.sh)" - "$USERNAME"
 
 if [[ $(uname -r) == *.amzn2.x86_64 ]]
 then
-    curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/ec2_init.sh "$USERNAME" | sudo bash
-elif [[ $(uname -r) == *.el7.x86_64 ]]
+    curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/ec2_init.sh "$USERNAME"
+elif [[ $(uname -r) == *.el8.x86_64 || *.el7.x86_64 ]]
 then
-    curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/centos_init.sh "$USERNAME" | sudo bash
+    curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/centos_init.sh "$USERNAME"
 else
     printf "This setup is not supported on this OS"
 fi
