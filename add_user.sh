a6fce4ccd532712c2f9fa899dfd4a46a88b6ee0e
Dharm Sonariya <dharmsonariya@outlook.com>
2020-06-10 23:20:33 +0000

    experimening to error in the add_user.sh

--- add_user.sh
+++ add_user.sh
@@ -19,15 +19,15 @@ usermod -aG wheel "$USERNAME"
 echo "User has been added to wheel group"
 
 if [[ $(uname -r) == *.amzn2.x86_64 ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_sudo_init.sh)" &&
+    "$(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_sudo_init.sh)" &&
     /bin/su -c "cd  /home/$USERNAME/ && sh -c $(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_user_init.sh)" - "$USERNAME" && 
     curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_ec2_init.sh "$USERNAME"
 elif [[ $(uname -r) == *.el7.x86_64 ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_sudo_init.sh)" &&
+    "$(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_sudo_init.sh)" &&
     /bin/su -c "cd  /home/$USERNAME/ && sh -c $(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_user_init.sh)" - "$USERNAME" && 
     curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_ws_init.sh "$USERNAME"
 elif [[ $(uname -r) == *.el8_0.x86_64 ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el8_sudo_init.sh)" &&
+    "$(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el8_sudo_init.sh)" &&
     /bin/su -c "cd  /home/$USERNAME/ && sh -c $(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el8_user_init.sh)" - "$USERNAME"
 else
     printf "This setup is not supported on this OS"
