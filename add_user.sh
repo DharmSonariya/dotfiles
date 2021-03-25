e7a64d47ba7cf7389f06cfc21a5177976c6fba38
Dharm Sonariya <dharm.sonariya@userful.com>
2021-03-24 18:46:16 -0600

    Removed CentOS 8

--- add_user.sh
+++ add_user.sh
@@ -26,9 +26,6 @@ elif [[ $(uname -r) == *.el7.x86_64 ]]; then
     curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_sudo_init.sh | sh &&
     /bin/su -c "cd  /home/$USERNAME/ && sh -c $(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_user_init.sh)" - "$USERNAME" && 
     curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_ws_init.sh "$USERNAME"
-elif [[ $(uname -r) == *.el8_0.x86_64 ]]; then
-    curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el8_sudo_init.sh | sudo sh &&
-    curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el8_user_init.sh | sudo -u $USERNAME sh
 else
     printf "This setup is not supported on this OS"
 fi
