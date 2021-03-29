1f633398223d1b9f22d4e1e8c9363acf89a3676a
Dharm Sonariya <dharm.sonariya@userful.com>
2021-03-28 20:27:36 -0600

    add the remaining scripts to execute to add_user.sh

--- add_user.sh
+++ add_user.sh
@@ -19,3 +19,7 @@ echo "User has been added to wheel group"
 
 cd /home/$USERNAME
 su $USERNAME
+
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_sudo_init.sh)" &&
+    curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_user_init.sh | sh &&
+    curl -fsSL https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/el7_ec2_init.sh | sh
