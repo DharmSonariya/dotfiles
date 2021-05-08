ee10d79f56fcfade2d2a5a52c871744b770275e4
Dharm Sonariya <dharm.sonariya@userful.com>
2021-05-07 23:33:48 -0600

    add urls to download scripts

--- install.sh
+++ install.sh
@@ -50,7 +50,10 @@ file_url="https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/all_do
 https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/all_dotfiles/.bash_profile 
 https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/all_dotfiles/.bash_prompt 
 https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/all_dotfiles/.bashrc 
-https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/all_dotfiles/.gitignore"
+https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/all_dotfiles/.gitignore
+https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/all_dotfiles/aws_sso.py
+https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/all_dotfiles/sso.sh
+https://raw.githubusercontent.com/DharmSonariya/dotfiles/master/all_dotfiles/upgrade.sh"
 
 for file in ${file_url}; do
     echo "Downloading $file in home directory."
